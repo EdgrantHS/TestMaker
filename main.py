d7856de7e3ba59dafa776b20460ac33ff85e40a8
EdgrantHS <edgrant.h@gmail.com>
2023-11-08 14:11:44 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from UnitTest import UnitTest as UT
+# from Extractor import Extractor as EX
+
+if __name__ == '__main__':
+    input_test = ["10 3 1 2 3", "25 5 10 10 10", "1 1 2", "10 4 10 0 0 0", "10 5 10 0 0 0 1", "10 5 10 0 0 0 0", "0 1 0", "0 0", "0 10 0 0 1", "1 1"]
+    expected_output = ["3", "2", "0", "4", "4", "5", "1", "0", "2", "1"]
+
+    checker = UT("code", input_test=input_test, output_test=expected_output)
+    checker.run()
